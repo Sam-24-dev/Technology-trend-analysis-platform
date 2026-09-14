6c6da5298aea40f0197ee68cf73aeb13762ed022
Sam-24-dev <Sam-24-dev@users.noreply.github.com>
2026-09-14 15:24:21 +0000

    deploy: 55ea1f1a03df5a63f8ae4db6eeac4222391b1f11

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,7 +39,7 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3369756627",
+    serviceWorkerVersion: "1034616876",
     timeoutMillis: 1000,
   },
   onEntrypointLoaded: async function(engineInitializer) {
